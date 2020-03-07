3b05f4440f4651131696f38c3dd3d46d962581a3
Yuanmao Zhu (Kris) <32859592+yuanmaoKris@users.noreply.github.com>
2020-03-07 12:56:02 -0700

    change all posts in same div

--- sitePjt/friendship/models.py
+++ sitePjt/friendship/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import Author
+from django.db.models import Q
 import uuid
 
 # Create your models here.
@@ -9,9 +10,13 @@ class FriendRequest(models.Model):
     author_from = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author_from')
     author_to = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author_to')
     is_approved = models.BooleanField(default="False")
+
+
+
     
 class Friendship(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     author_a = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author_a')
     author_b = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author_b')
     
+
