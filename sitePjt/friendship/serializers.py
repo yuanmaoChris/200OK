c600018884a9d2233ccdd4f9b1af59d413f4b83e
Leo Hong <33166820+returnturn@users.noreply.github.com>
2020-03-10 14:39:57 -0600

    serializer

---
+++ sitePjt/friendship/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import *
+
+
+class FriendRequestSerializer(serializers.ModelSerializer):
+
+	pub_date = serializers.DateTimeField('date posted', auto_now_add=True, blank=True)
+    is_approved = serializers.BooleanField(default="False")
+
+	class Meta:
+		model = FriendRequest
+		fields = ('author_from', 'author_to', 'pub_date', 'is_approved')
+
+class FriendshipSerializer(serializers.ModelSerializer):
+
+	id = serializers.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+	class Meta:
+		model = FriendRequest
+		fields = ('id', 'author_a', 'author_b')
+
