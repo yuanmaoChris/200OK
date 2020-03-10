fb914085cf69af0d6233a46e13702210f05c1e20
Leo Hong <33166820+returnturn@users.noreply.github.com>
2020-03-10 14:39:02 -0600

    serializer

---
+++ sitePjt/accounts/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import *
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+
+	id = serializers.SerializerMethodField('get_id')
+
+	def get_id(self, obj):
+		return "http://service/author/" + obj.uuid_str
+
+	class Meta:
+		model = Post
+		fields = ('id', 'host', 'displayName', 'url', 'content', 'github', 'bio')
