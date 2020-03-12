be65d65511335d3895589633518a3f3b5ad1f41d
Leo Hong <33166820+returnturn@users.noreply.github.com>
2020-03-12 15:10:56 -0600

    Update views.py

--- sitePjt/accounts/views.py
+++ sitePjt/accounts/views.py
@@ -50,7 +50,7 @@ def register_view(request):
             login(request, new_user)
             if next:
                 return redirect(next)
-            return redirect('/service/posts/')
+            return redirect('/posts/')
     else:
         form = UserCreationForm()
     context = {
